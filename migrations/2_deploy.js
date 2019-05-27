c6d4da8cf6745a6f2ac397e3479e4bd179c3188f
Andrew B Coathup <andrew@coathup.net>
2019-05-27 18:38:34 +1000

    Initial commit

---
+++ migrations/2_deploy.js
@@ -0,0 +1,13 @@
+const Simple777Token = artifacts.require('Simple777Token');
+
+require('openzeppelin-test-helpers/configure')({ web3 });
+
+const { singletons } = require('openzeppelin-test-helpers');
+
+module.exports = async function(deployer, network, accounts) {
+  if (network == "development") {
+    const erc1820 = await singletons.ERC1820Registry(accounts[0]);
+  }
+
+  deployer.deploy(Simple777Token);
+};
