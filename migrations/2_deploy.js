7ce1ef938be1b432cf2b243528335ca8987a89a2
Andrew B Coathup <andrew@coathup.net>
2019-05-29 11:42:19 +1000

    Add note on ERC1820 registry in test environments

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -6,6 +6,7 @@ const { singletons } = require('openzeppelin-test-helpers');
 
 module.exports = async function (deployer, network, accounts) {
   if (network === 'development') {
+    // In a test environment an ERC777 token requires deploying an ERC1820 registry
     await singletons.ERC1820Registry(accounts[0]);
   }
 
