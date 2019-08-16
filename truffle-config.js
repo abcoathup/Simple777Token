06e62cc590a1314981b390372c34bea048b42a73
Andrew B Coathup <andrew@coathup.net>
2019-08-16 13:48:07 +1000

    Add kovan

--- truffle-config.js
+++ truffle-config.js
@@ -76,6 +76,15 @@ module.exports = {
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
 
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, 'https://kovan.infura.io/v3/' + infuraProjectID),
+      network_id: 42, // eslint-disable-line camelcase
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
